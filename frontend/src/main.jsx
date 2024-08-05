e7f53fd1974e8fb01eb3b48ac3361db831acfc3a
Bilha Mwengi <bilhamwengi34@gmail.com>
2024-08-05 15:31:58 +0300

    shop context

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,12 @@
+// eslint-disable-next-line no-unused-vars
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import ShopContextProvider from './context/ShopContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <ShopContextProvider>
     <App />
-  </React.StrictMode>,
+  </ShopContextProvider>
 )
